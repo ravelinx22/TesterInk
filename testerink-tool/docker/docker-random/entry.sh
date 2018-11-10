3b18c1a6883704ca519f000b85f362fb269fe637
William Ravelo <drummerwilliam@gmail.com>
2018-11-10 00:40:19 -0500

    fix wait host sh

--- testerink-tool/docker/docker-random/entry.sh
+++ testerink-tool/docker/docker-random/entry.sh
@@ -4,26 +4,18 @@ set -e
 
 TIMEOUT=10000
 
-is_integer() {
-  test "$1" -eq "$1" 2> /dev/null
-}
-
 connect_to_service() {
   nc -w 1 -z "$1" "$2"
 }
 
 wait_for_service() {
-  local host="${1%:*}"
-  local port="${1#*:}"
+  local host="localhost"
+  local port="5555"
   local output
-  if ! is_integer "$port"; then
-    printf 'Error: "%s" is not a valid host:port combination.\n' "$1" >&2
-    return 1
-  fi
-  printf 'Waiting for %s to become available ... ' "$1" >&2
+  printf 'Waiting for %s to become available ... ' "localhost:5555" >&2
   # shellcheck disable=SC2155
   local timeout=$(($(date +%s)+TIMEOUT))
-  while ! output="$(connect_to_service "$host" "$port" 2>&1)"; do
+  while ! output="$(connect_to_service "localhost" "5555" 2>&1)"; do
     if [ "$(date +%s)" -gt "$timeout" ]; then
       echo 'timeout' >&2
       if [ ! -z "$output" ]; then
@@ -37,19 +29,7 @@ wait_for_service() {
 }
 
 while [ $# != 0 ]; do
-  if [ "$1" = '-t' ] || [ "$1" = '--timeout' ]; then
-    if ! is_integer "$2"; then
-      printf 'Error: "%s" is not a timeout integer.\n' "$2" >&2
-      exit 1
-    fi
-    TIMEOUT="$2"
-    shift 2
-  fi
-  if [ "$1" = '--' ]; then
-    shift
-    exec "$@"
-  fi
-  wait_for_service "$1"
+  wait_for_service "localhost:5555"
   shift
 done
 
