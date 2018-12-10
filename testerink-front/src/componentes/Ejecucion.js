593422c920215b064573cbc43583f093048cb012
John William Sanabria Delgado <John_Sanabria@bvc.com.co>
2018-12-10 05:10:40 -0500

    cambios en el front

--- testerink-front/src/componentes/Ejecucion.js
+++ testerink-front/src/componentes/Ejecucion.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom';
+import { Progress } from 'react-sweet-progress';
+import "react-sweet-progress/lib/style.css"
 import { create } from 'domain';
 
 class Ejecucion extends Component {
@@ -20,9 +22,22 @@ class Ejecucion extends Component {
                 <td>{(new Date(create_date)).toLocaleDateString()}</td>
                 <td>{_id}</td>
                 <td>{nombre}</td>
-                <td>{state}</td>
                 <td>
-                    <Link to={`/resultadoPruebas/${_id}`} className="btn btn-success">Resultados</Link>
+                { state ==='Pending' && 
+                    <Progress percent={50} status="active" />
+                }
+                {state === 'Executed' && 
+                    <Progress percent={100} status="success" />
+                }
+                {state === 'Register' && 
+                    <Progress percent={0} status="active" />                   
+                }
+                </td>
+                
+                <td>
+                    {state === 'Executed' && 
+                        <Link to={`/resultadoPruebas/${_id}`} className="btn btn-success">Resultados</Link>
+                    }
                 </td>
             </tr>
          );
